86626f8c171678fb435bbb050e88b3a0672a7bdb
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-26 22:59:00 +0530

    added machinery setup

--- core/static/core/js/welcome.js
+++ core/static/core/js/welcome.js
@@ -1,25 +1,12 @@
-const progressCircle = document.querySelector(".autoplay-progress svg");
-const progressContent = document.querySelector(".autoplay-progress span");
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop:true,
-    autoplay: {
-        delay: 2500, slidesPerView: 1,
-        disableOnInteraction: false
-    },
-    pagination: {
-        el: ".mySwiper-swiper-pagination",
-        clickable: true
-    },
-    navigation: {
-        nextEl: ".mySwiper-swiper-button-next",
-        prevEl: ".mySwiper-swiper-button-prev"
-    },
-    on: {
-        autoplayTimeLeft(s, time, progress) {
-            progressCircle.style.setProperty("--progress", 1 - progress);
-            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-        }
-    }
-});
+window.onscroll = function() {stickNavbar()};
+
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
+
+function stickNavbar() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
